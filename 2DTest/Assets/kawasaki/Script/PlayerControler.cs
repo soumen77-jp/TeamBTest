260366cf9886d9bcd9bbb02f60a6d30846af9dde
kawasakiryota <h1202419005@hiratagakuen.onmicrosoft.com>
2024-10-09 12:32:02 +0900

    プレイヤーのスクリプト作成、絵の追加

---
+++ 2DTest/Assets/kawasaki/Script/PlayerControler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed = 5.0f;  //移動スピード
+    float axisH;                //縦軸
+    float axisV;                //横軸
+    Rigidbody2D rbody;          //Rigidbody2D
+                                //
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();    //Rigidbody2Dを得る
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        axisH = Input.GetAxisRaw("Horizontal");     //左右キー入力
+        axisV = Input.GetAxisRaw("Vertical");       //上下キー入力
+    }
+
+    private void FixedUpdate()
+    {
+        //速度更新
+        rbody.velocity = new Vector2(axisH, axisV).normalized * speed;
+    }
+}
